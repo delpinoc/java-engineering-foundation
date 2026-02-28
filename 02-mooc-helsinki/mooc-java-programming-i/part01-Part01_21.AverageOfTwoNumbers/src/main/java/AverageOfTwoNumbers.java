6839fbea6347dc4fdc1f530fdb801c5196b9c731
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-02-28 20:44:57 +0100

    feat(mooc/part1): apply type promotion and floating-point division for accurate average calculation

--- 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_21.AverageOfTwoNumbers/src/main/java/AverageOfTwoNumbers.java
@@ -7,6 +7,13 @@ public class AverageOfTwoNumbers {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("The average is " + ((double)(firstNumber + secondNumber)/2));
+        
     }
 }
