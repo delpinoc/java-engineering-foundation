88c12752f871bf7a553348f8ded6e0b2ec88f7d3
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-02-28 21:33:23 +0100

    feat(mooc/part1): construct a multi-operation calculator for basic arithmetic

--- 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_23.SimpleCalculator/src/main/java/SimpleCalculator.java
@@ -7,6 +7,20 @@ public class SimpleCalculator {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        
+        int suma = firstNumber + secondNumber;
+        int resta = firstNumber - secondNumber;
+        int multiplicacion = firstNumber * secondNumber;
+        double division = (double)firstNumber/secondNumber;
+        
+        System.out.println(firstNumber + " + " + secondNumber + " = " + suma);
+        System.out.println(firstNumber + " - " + secondNumber + " = " + resta);
+        System.out.println(firstNumber + " * " + secondNumber + " = " + multiplicacion);
+        System.out.println(firstNumber + " / " + secondNumber + " = " + division);
     }
 }
