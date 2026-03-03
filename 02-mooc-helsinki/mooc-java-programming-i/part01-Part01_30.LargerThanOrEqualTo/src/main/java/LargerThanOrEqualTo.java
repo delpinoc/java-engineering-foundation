8cd99bf2708e313fb984c8abe8753f2164ee0ea8
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-03 10:15:59 +0100

    feat(mooc/part1): implement multi-branch comparison to determine maximum value or equality

--- 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part01-Part01_30.LargerThanOrEqualTo/src/main/java/LargerThanOrEqualTo.java
@@ -5,6 +5,23 @@ public class LargerThanOrEqualTo {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Give the first number:");
+        int firstNumber = Integer.valueOf(scan.nextLine());
+        
+        System.out.println("Give the second number:");
+        int secondNumber = Integer.valueOf(scan.nextLine());
+        
+        int greaterNumber = 0;
+        if (firstNumber == secondNumber) {
+            System.out.println("The numbers are equal!");
+        } else if (firstNumber > secondNumber) {
+            greaterNumber = firstNumber;
+            System.out.println("Greater number is: " + greaterNumber);
+        } else {
+            greaterNumber = secondNumber;
+            System.out.println("Greater number is: " + greaterNumber);
+        }
 
     }
 }
