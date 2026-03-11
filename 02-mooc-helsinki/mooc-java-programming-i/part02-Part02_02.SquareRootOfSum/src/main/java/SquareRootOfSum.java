342d722f2a6e23383b05a0badec0ae15c21caee7
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-11 15:06:41 +0100

    feat(mooc/part2): compute square root of summed integers using mathematical utility functions

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,13 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        
+        double squareRoot = Math.sqrt(firstNumber + secondNumber);
+        
+        System.out.println(squareRoot);
 
     }
 }
