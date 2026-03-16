a7e3f80fbd0d6852f89f04cd44a4fd55c861332d
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-16 23:10:28 +0100

    feat(mooc/part2): implement iterative numerical summation using external state accumulator

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,24 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int totalSum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number < 0 || number > 0){
+                totalSum += number;
+            }
+        }
+        
+        System.out.println("Sum of the numbers: " + totalSum);
+        
 
     }
 }
