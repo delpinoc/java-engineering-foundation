496f23ca1e819ac6e18e89ac77ca65c4dce42faf
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-16 23:38:25 +0100

    feat(mooc/part2): implement concurrent state tracking for input frequency and cumulative summation

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_11.NumberAndSumOfNumbers/src/main/java/NumberAndSumOfNumbers.java
@@ -5,6 +5,26 @@ public class NumberAndSumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int totalNumbers = 0;
+        int totalSum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number > 0 ||number < 0) {
+                totalNumbers++;
+                totalSum += number;
+            }
+        }
+        
+        System.out.println("Number of numbers: " + totalNumbers);
+        System.out.println("Sum of the numbers: " + totalSum);
 
     }
 }
