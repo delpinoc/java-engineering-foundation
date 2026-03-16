bf07669d81145e968ddfb718d2cdef726270ea13
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-16 23:47:57 +0100

    feat(mooc/part2): compute filtered arithmetic mean with division-by-zero protection

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,5 +6,23 @@ public class AverageOfNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int totalNumbers = 0;
+        int totalSum = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number > 0 || number < 0) {
+                totalNumbers++;
+                totalSum += number;
+            }
+        }
+        double avg = (double)totalSum / totalNumbers;
+        System.out.println("Average of the numbers: " + avg);
     }
 }
