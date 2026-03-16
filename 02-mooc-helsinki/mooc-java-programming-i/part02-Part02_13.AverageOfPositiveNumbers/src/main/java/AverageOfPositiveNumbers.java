bb71c76d847197f7bfa978b5fe4a29ea89a15269
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-17 00:03:55 +0100

    feat(mooc/part2): compute arithmetic mean of positive input subset with empty state handling

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,31 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int totalPositiveNumbers = 0;
+        int totalPositiveSum = 0;
+        
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number > 0) {
+                totalPositiveNumbers++;
+                totalPositiveSum += number;
+            }
+            
+            
+        }
+        if (totalPositiveNumbers > 0) {
+            double avg = totalPositiveSum / (double) totalPositiveNumbers;
+            System.out.println(avg); 
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
+        
 
     }
 }
