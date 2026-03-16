1aa4b0d66692a8cd839669c8638574eac14ac7fe
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-16 22:48:52 +0100

    feat(mooc/part2): implement iterative input counting using external state variable

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,23 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int totalNumbers = 0;
+        
+        while (true) {
+            System.out.println("Give a number:");
+            int actualNumber = Integer.valueOf(scanner.nextLine());
+            
+            if (actualNumber == 0) {
+                break;
+            }
+            
+            if (actualNumber < 0 || actualNumber > 0) {
+                totalNumbers++;
+            }
+        }
+        
+        System.out.println("Number of numbers: " + totalNumbers);
 
     }
 }
