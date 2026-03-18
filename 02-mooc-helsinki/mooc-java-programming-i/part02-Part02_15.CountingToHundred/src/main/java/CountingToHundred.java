2a9d4dfb74ad8b7e51988f22369782bfc16fd097
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-18 14:59:03 +0100

    feat(mooc/part2): implement bounded iteration with dynamic lower bound and fixed upper limit

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -5,6 +5,12 @@ public class CountingToHundred {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int number = Integer.valueOf(scanner.nextLine());
+        
+        for (int actualNumber = number; actualNumber <= 100; actualNumber++) {
+            System.out.println(actualNumber);
+        }
 
     }
 }
