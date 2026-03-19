f08366136db93283ac217759e1faa20a85e6dbf6
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-19 09:00:15 +0100

    feat(mooc/part2): compute arithmetic series summation across dual dynamic boundaries

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,20 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("First number?");
+        int lowerLimit = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Last number?");
+        int upperLimit = Integer.valueOf(scanner.nextLine());
+        
+        int result = 0;
+        
+        for (int i = lowerLimit; i <= upperLimit; i++) {
+            result += i;
+        }
+        
+        System.out.println("The sum is: " + result);
 
     }
 }
