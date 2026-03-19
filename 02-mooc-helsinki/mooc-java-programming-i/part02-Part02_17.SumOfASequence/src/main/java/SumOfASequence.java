9fe89f126b3ea949ee668763ed0e3ce1f4292db8
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-19 08:54:03 +0100

    feat(mooc/part2): compute arithmetic series summation using bounded iterative accumulation

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_17.SumOfASequence/src/main/java/SumOfASequence.java
@@ -5,6 +5,16 @@ public class SumOfASequence {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Last number?");
+        int lastNumber = Integer.valueOf(scanner.nextLine());
+        
+        int result = 0;
+        for (int i = 1; i <= lastNumber; i++) {
+            result += i;
+        }
+        
+        System.out.println("The sum is " + result);
 
     }
 }
