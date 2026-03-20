111c880d4aeb919008d834a0e35254cceaafb175
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-20 19:54:20 +0100

    feat(mooc/part2): implement parameterized method for modulo-based sequence filtering within dynamics boundaries

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,22 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Enter the lower limit:");
+        int lowerLimit = Integer.valueOf(scanner.nextLine());
+        
+        System.out.println("Enter the upper limit:");
+        int upperLimit = Integer.valueOf(scanner.nextLine());
+        divisibleByThreeInRange(lowerLimit, upperLimit);
+        
+    }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
     }
 
 }
