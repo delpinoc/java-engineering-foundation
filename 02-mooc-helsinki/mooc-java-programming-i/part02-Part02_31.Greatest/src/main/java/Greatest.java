dfeff51e7e9dc2d4e241928283f1d33eb470e1f7
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-03-21 23:08:42 +0100

    implement multi-parameter method for complex conditional evaluation and maximum value extraction

--- 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -3,8 +3,17 @@ public class Greatest {
 
     public static int greatest(int number1, int number2, int number3) {
         //write some code here
-
-        return -1;
+        int greatestNumber = number1;
+        
+        if (number2 > greatestNumber) {
+            greatestNumber = number2;
+        }
+        
+        if (number3 > greatestNumber) {
+            greatestNumber = number3;
+        }
+        
+        return greatestNumber;
     }
 
     public static void main(String[] args) {
