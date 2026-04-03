46ee1e4d815f1232301528ef6fe9060bc3ad4d6c
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-03 22:12:16 +0200

    feat(mooc/part3): implement linear search to locate the index of the minimun element

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_11.IndexOfSmallest/src/main/java/IndexOfSmallest.java
@@ -7,13 +7,37 @@ public class IndexOfSmallest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Integer> numbers = new ArrayList<>();
         // implement here a program that reads user input
         // until the user enters 9999
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == 9999) {
+                break;
+            }
+            
+            numbers.add(input);
+        }
         
         // after that, the program prints the smallest number
+        int smallestNumber = numbers.get(0);
+        
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) < smallestNumber) {
+                smallestNumber = numbers.get(i);
+            }
+        }
+        
+        System.out.println("Smallest number: " + smallestNumber);
         // and its index -- the smallest number
         // might appear multiple times
-
+        
+        for (int i = 0; i < numbers.size(); i++) {
+            if (numbers.get(i) == smallestNumber) {
+                System.out.println("Found at index: " + i);
+            }
+        }
+        
         
     }
 }
