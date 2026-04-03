e440dffa3c6dabdce65b596b81c6fb8452e90ff5
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-03 21:59:08 +0200

    feat(mooc/part3): implement linear search algorithm to extract maximum value from a dynamic list

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -20,5 +20,14 @@ public class GreatestInList {
         System.out.println("");
 
         // implement finding the greatest number in the list here
+        int maxValue = list.get(0);
+        
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i) > maxValue) {
+                maxValue = list.get(i);
+            }
+        }
+        
+        System.out.println("The greatest number: " + maxValue);
     }
 }
