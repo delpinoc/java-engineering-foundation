1f971f577d351435766bfd7ebafb7b174f358d86
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-04 17:56:13 +0200

    feat(mooc/part3): compute floating-point average via accumulator pattern and dynamic size evaluation

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_13.AverageOfAList/src/main/java/AverageOfAList.java
@@ -9,8 +9,31 @@ public class AverageOfAList {
 
         // implement here a program, that first reads user input
         // adding them on a list until user gives -1.
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        while (true) {
+            int inputNumber = Integer.valueOf(scanner.nextLine());
+            
+            if (inputNumber == -1){
+                break;
+            }
+            
+            numbers.add(inputNumber);
+        }
+        
         // Then it computes the average of the numbers on the list
         // and prints it.
+        
+        int totalSum = 0;
+        
+        for (int currentNumber : numbers) {
+            totalSum += currentNumber;
+        }
+        
+        double average = (double)totalSum / numbers.size();
+        
+        System.out.println("Average: " + average);
+        
         
     }
 }
