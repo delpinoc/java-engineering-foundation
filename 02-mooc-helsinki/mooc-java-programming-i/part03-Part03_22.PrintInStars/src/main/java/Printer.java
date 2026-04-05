20d77ac4494a4175c2f6121c812f83802c70ce84
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-05 22:40:48 +0200

    feat(mooc/part3): implement text-based histogram generation for integer array visualization

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_22.PrintInStars/src/main/java/Printer.java
@@ -9,6 +9,12 @@ public class Printer {
 
     public static void printArrayInStars(int[] array) {
         // Write some code in here
+        for (int numberOfStars : array) {
+            for (int i = 0; i < numberOfStars; i++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
     }
 
 }
