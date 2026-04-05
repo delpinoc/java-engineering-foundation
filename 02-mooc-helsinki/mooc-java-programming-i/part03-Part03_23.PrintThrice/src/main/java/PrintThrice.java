66392daa324f9efb6f298ee4724cd35f4392ecd5
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-05 22:56:51 +0200

    feat(mooc/part3): implement consecutive string concatenation and sequential output

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -7,6 +7,17 @@ public class PrintThrice {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
+        System.out.println("Give a word:");
+        String myWord = scanner.nextLine();
+        
+        printWord(myWord, 3);
+        
 
+    }
+    
+    public static void printWord(String word, int limit) {
+        for (int i = 0; i < limit; i++) {
+            System.out.print(word + "");
+        }
     }
 }
