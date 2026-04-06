71e58936daf70508f2a928514279ef45ce814ccf
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 17:25:29 +0200

    feat(mooc/part3): implement basic user authentication via compound string validation

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_25.Login/src/main/java/Login.java
@@ -5,6 +5,43 @@ public class Login {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        // users
+        String[] usernames = {"alex", "emma"};
+        
+        // passwords
+        String[] passwords = {"sunshine", "haskell"};
+        
+        System.out.println("Enter username:");
+        String enteredUsername = scanner.nextLine();
+        
+        System.out.println("Enter password:");
+        String enteredPassword = scanner.nextLine();
+        
+        if (login(usernames, passwords, enteredUsername, enteredPassword)) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+        
 
+    }
+    
+    public static boolean login(String[] usernames, String[] passwords, String username, String password) {   
+        boolean isCorrect = false;
+        for (int i = 0; i < usernames.length; i++) {
+            if (checkUsername(usernames[i], username) && checkPassword(passwords[i], password)) {
+                isCorrect = true;
+            }
+        }
+        return isCorrect;
+    }
+
+    public static boolean checkUsername(String realUsername, String enteredUsername) {
+        return realUsername.equals(enteredUsername);
+    }
+
+    public static boolean checkPassword(String realPass, String enteredPass) {
+        return realPass.equals(enteredPass);
     }
 }
