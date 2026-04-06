b80b1ac5cebaaf0b4394203a18c504d66f08bb02
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 18:39:21 +0200

    feat(mooc/part3): implement dynamic array indexing to extract terminal tokens from parsed strings

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,18 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String splitter = " ";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] parts = input.split(splitter);
+            
+            System.out.println(parts[parts.length-1]);
+        }
     }
 }
