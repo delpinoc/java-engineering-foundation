98d15e958ac9569bb8fafff19ad1c67a6501fab5
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 16:54:51 +0200

    feat(mooc/part3): implement literal string comparison for conditional state evaluation

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_24.IsItTrue/src/main/java/IsItTrue.java
@@ -5,6 +5,15 @@ public class IsItTrue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        String rightAnswer = "true";
+        System.out.println("Give a string:");
+        String answer = scanner.nextLine();
+        
+        if (answer.equals(rightAnswer)) {
+            System.out.println("You got it right!");
+        } else {
+            System.out.println("Try again!");
+        }
     }
 }
