3a5232ec162c74b758cd7c00961ca1850514978b
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 18:35:13 +0200

    feat(mooc/part3): implement targeted extraction of initial tokens from parsed strings

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -5,7 +5,19 @@ public class FirstWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String splitter = " ";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] parts = input.split(splitter);
+            System.out.println(parts[0]);
+        }
+        
 
     }
 }
