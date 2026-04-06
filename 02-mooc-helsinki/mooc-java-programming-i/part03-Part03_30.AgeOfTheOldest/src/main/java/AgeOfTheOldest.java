5387c4963507f055658e0e452dc1c426fbc47286
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 18:58:05 +0200

    feat(mooc/part3): implement string parsing and type conversion for numeric data extraction

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,24 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
+        String splitter = ",";
+        
+        int oldest = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] parts = input.split(splitter);
+            int currentAge = Integer.valueOf(parts[1]);
+            
+            if (oldest < currentAge) {
+                oldest = currentAge;
+            }
+        }
+        
+        System.out.println("Age of the oldest: " + oldest);
     }
 }
