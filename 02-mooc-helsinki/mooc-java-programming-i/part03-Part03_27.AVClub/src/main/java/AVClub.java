a985233fd7936bb3262ae3133904204409a50f55
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 18:17:58 +0200

    feat(mooc/part3): implement conditional token extraction via substring matching

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -1,11 +1,29 @@
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class AVClub {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String wordToSearch = "av";
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] foundWords = input.split(" ");
+            
+            for (String currentWord : foundWords) {
+                if (currentWord.contains(wordToSearch)) {
+                    System.out.println(currentWord);
+                }
+            }
+            
+        }
 
     }
 }
