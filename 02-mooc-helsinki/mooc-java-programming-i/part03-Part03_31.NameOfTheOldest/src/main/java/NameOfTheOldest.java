206e228a74487a6dcd1b65580e6619b5af16788f
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 19:50:45 +0200

    feat(mooc/part3): implement parallel state tracking to extract associated string data based on numeric maximums

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,28 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String splitter = ",";
+        
+        int oldest = -1;
+        String nameOfOldestPerson = "";
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] dataOfPerson = input.split(splitter);
+            int age = Integer.valueOf(dataOfPerson[1]);
+            
+            if (age > oldest) {
+                oldest = age;
+                nameOfOldestPerson = dataOfPerson[0];
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + nameOfOldestPerson);
+        
 
     }
 }
