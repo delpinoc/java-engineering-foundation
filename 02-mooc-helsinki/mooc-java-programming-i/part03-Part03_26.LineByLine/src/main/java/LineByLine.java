c1f47dd6492ac74b612a3e7b78f27aca12a9d4b2
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 17:40:33 +0200

    feat(mooc/part3): implement delimiter-based string parsing and sequential token extraction

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,22 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")){
+                break;
+            }
+            
+            String[] cadenas = input.split(" ");
+            
+            for (String currentString : cadenas) {
+                System.out.println(currentString);
+            }
+        }
+        
+        
+        
 
     }
 }
