cb01e3db5318d940ba9a7cdd6150b46c12227aa8
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-06 20:14:23 +0200

    feat(mooc/part3): implement logic to find longest string and calculate average from formatted input

--- 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,6 +6,36 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String splitter = ",";
+        
+        String longestName = "";
+        double sumOfBirths = 0;
+        int count = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] dataOfPerson = input.split(splitter);
+            String name = dataOfPerson[0];
+            int yearOfBirth = Integer.valueOf(dataOfPerson[1]);
+            
+            sumOfBirths += yearOfBirth;
+            count++;
+            
+            if (name.length() > longestName.length()) {
+                longestName = name;
+            }
+            
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + ((double)sumOfBirths / count));
+        
+        
 
 
     }
