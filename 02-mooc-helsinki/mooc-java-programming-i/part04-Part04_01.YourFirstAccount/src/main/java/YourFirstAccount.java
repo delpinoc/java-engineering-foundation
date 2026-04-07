a48f299acd4a5b56959fed21d59c6b7529edc478
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-04-07 22:47:58 +0200

    feat(mooc/part4): implement initial object instantiation and state mutation for Account entity

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -4,5 +4,9 @@ public class YourFirstAccount {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // Write your program here
+        Account jorgeAccount = new Account("Jorge's account", 100.0);
+        jorgeAccount.deposit(20);
+        System.out.println(jorgeAccount);
+        
     }
 }
