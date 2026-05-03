414766d263e6fb7d69eb06332ca514958be851bc
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-03 18:24:29 +0200

    feat(mooc/part4): simulate transactional state transfer between distinct object instances

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_02.YourFirstBankTransfer/src/main/java/YourFirstBankTransfer.java
@@ -4,5 +4,14 @@ public class YourFirstBankTransfer {
     public static void main(String[] args) {
         // Do not touch the code in Account.java
         // write your program here
+        Account cuenta = new Account("Matthews account", 1000);
+        Account cuenta2 = new Account("My account", 0);
+        
+        cuenta.withdrawal(100);
+        cuenta2.deposit(100);
+        
+        System.out.println(cuenta);
+        System.out.println(cuenta2);
+        
     }
 }
