b35cd2b82a5a4b399ef72912f0af236194d2588e
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-05 18:10:30 +0200

    feat(mooc/part4): implement state aggregation and multi-instance data tracking for Statistics entity

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,25 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics statistics = new Statistics();
+        Statistics evenStatistics = new Statistics();
+        Statistics oddStatistics = new Statistics();
+        
+        System.out.println("Enter numbers:");
+        int actualNumber = 0;
+        while (actualNumber != -1) {
+            actualNumber = Integer.valueOf(scanner.nextLine());
+            if (actualNumber != -1) {
+                statistics.addNumber(actualNumber);
+                if (actualNumber % 2 != 0) {
+                    oddStatistics.addNumber(actualNumber);
+                } else {
+                    evenStatistics.addNumber(actualNumber);
+                }
+            }
+        }
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Sum of even numbers: " + evenStatistics.sum());
+        System.out.println("Sum of odd numbers: " + oddStatistics.sum());
     }
 }
