e7808e746b6079ec2b06987202ddcabadebeaf19
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 21:58:55 +0200

    feat(mooc/part4): implement dynamic file access via user-defined resource path resolution

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -6,6 +6,19 @@ public class PrintingASpecifiedFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Which file should have its contents printed?");
+        String ruta = scanner.nextLine();
+        
+        try (Scanner scFile = new Scanner(Paths.get(ruta))) {
+            while (scFile.hasNextLine()) {
+                String row = scFile.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        
     }
 }
