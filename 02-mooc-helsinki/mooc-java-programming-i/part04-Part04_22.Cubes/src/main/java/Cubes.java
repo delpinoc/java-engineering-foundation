e3b4c6042253a6368b5dfaa509d43bfc5ab322b8
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 21:20:03 +0200

    feat(mooc/part4): implement real-time numerical transformation and parsing logic

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -6,5 +6,16 @@ public class Cubes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String row = scanner.nextLine();
+            
+            if (row.equalsIgnoreCase("end")) {
+                break;
+            }
+            
+            int number = Integer.valueOf(row);
+            int cubo = number * number * number;
+            System.out.println(cubo);
+        }
     }
 }
