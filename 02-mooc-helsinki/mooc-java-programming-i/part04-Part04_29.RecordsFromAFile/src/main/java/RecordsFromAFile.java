bf7bd1e5e5567d7c9c7d136eb6aa445a75d13670
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 23:24:39 +0200

    feat(mooc/part4): implement CSV-style data parsing and structured record extraction from external files

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -6,6 +6,26 @@ public class RecordsFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+        
+        try (Scanner scFile = new Scanner(Paths.get(file))) {
+            while (scFile.hasNextLine()) {
+                String row = scFile.nextLine();
+                
+                String[] personalData = row.split(",");
+                String name = personalData[0];
+                int age = Integer.valueOf(personalData[1]);
+                
+                if (age == 1) {
+                    System.out.println(personalData[0] + ", age: " + personalData[1] + " year");
+                } else {
+                    System.out.println(personalData[0] + ", age: " + personalData[1] + " years");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
