fbca9720976e10bebc7a9174e762ffddc9078ca6
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 22:41:24 +0200

    feat(mooc/part4): implement robust file-based lookup with exception safety and existence verification

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,24 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
+        
+        boolean isFound = false;
+        try (Scanner scFile = new Scanner(Paths.get(file))) {
+            while (scFile.hasNextLine()) {
+                String row = scFile.nextLine();
+                if (row.equals(searchedFor)) {
+                    isFound = true;
+                }
+            }
+            
+            if (isFound) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found!");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
 
     }
 }
