fc6798afc5e7efdc4a9d83ac0e74af618c2a4a87
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 18:54:48 +0200

    feat(mooc/part4): implement conditional output formatting for Book entities within dynamic collections

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_20.Books/src/main/java/Main.java
@@ -7,6 +7,37 @@ public class Main {
         
         // implement here the program that allows the user to enter 
         // book information and to examine them
-
+        ArrayList<Book> books = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Title:");
+            String title = sc.nextLine();
+            
+            if (title.isEmpty()) {
+                break;
+            }
+            
+            System.out.println("Pages:");
+            int numberOfPages = Integer.valueOf(sc.nextLine());
+            
+            System.out.println("Publication year:");
+            int publicationYear = Integer.valueOf(sc.nextLine());
+            
+            books.add(new Book(title, numberOfPages, publicationYear));
+        }
+        
+        System.out.println("What information will be printed?");
+        String answer = sc.nextLine();
+        
+        if (answer.equalsIgnoreCase("everything")) {
+            for (Book currentBook : books) {
+                System.out.println(currentBook);
+            }
+        } else {
+            for (Book currentBook : books) {
+                System.out.println(currentBook.getTitle());
+            }
+        }
     }
 }
