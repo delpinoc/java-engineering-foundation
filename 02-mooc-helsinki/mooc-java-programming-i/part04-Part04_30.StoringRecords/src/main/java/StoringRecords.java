7d9cbf902cfb1b19358896ce3b7540627a00b53c
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 23:55:23 +0200

    feat(mooc/part4): implement object hydration from structured file data into class-based collections

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,6 +25,18 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
+        try (Scanner scFile = new Scanner(Paths.get(file))) {
+            while (scFile.hasNextLine()) {
+                String row = scFile.nextLine();
+                
+                String[] parts = row.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
         return persons;
 
     }
