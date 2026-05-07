38aee18eab8c6d7ebcd3d42a6787162bfd787c0f
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 21:11:13 +0200

    feat(mooc/part4): implement stream-based input counting with sentinel termination

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,18 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int contador = 0;
+        while (true) {
+            String word = scanner.nextLine();
+            
+            if (word.equalsIgnoreCase("end")) {
+                break;
+            }
+            contador++;
+        }
+        
+        System.out.println(contador);
 
     }
 }
