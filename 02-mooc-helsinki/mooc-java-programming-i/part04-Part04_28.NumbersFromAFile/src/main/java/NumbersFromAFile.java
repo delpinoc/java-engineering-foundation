50cf543ad9e645c2797cf807475558eee5c7b579
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 22:51:16 +0200

    feat(mooc/part4): implement range-based numericcxal filtering and aggregation on file-sourced data

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -7,12 +7,27 @@ public class NumbersFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("File? ");
+        System.out.println("File? ");
         String file = scanner.nextLine();
-        System.out.print("Lower bound? ");
+        System.out.println("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
-        System.out.print("Upper bound? ");
+        System.out.println("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+        
+        int counter = 0;
+        
+        try (Scanner scFile = new Scanner(Paths.get(file))) {
+            while (scFile.hasNextLine()) {
+                int number = Integer.valueOf(scFile.nextLine());
+                if (number >= lowerBound && number <= upperBound) {
+                    counter++;
+                }
+            }
+            
+            System.out.println("Numbers: " + counter);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
 
     }
 
