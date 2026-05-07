5f449e6b489465baffeb6e0996993d8806cd8e6f
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-07 21:48:11 +0200

    feat(mooc/part4): implement file systema data ingestion and stream-to-console logic

--- 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part04-Part04_24.PrintingAFile/src/main/java/PrintingAFile.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class PrintingAFile {
 
     public static void main(String[] args) {
-
+        try (Scanner sc = new Scanner(Paths.get("data.txt"))) {
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                System.out.println(row);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
