693cfb961a033d10aed67ff02c0a7b4b953d0bfe
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-12 13:26:32 +0200

    feat(mooc/part5): implement comprehensive method and constructor overloading for Counter entity

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,21 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter contador = new Counter();
+        Counter contador2 = new Counter(10);
+        
+        System.out.println(contador);
+        System.out.println(contador2);
+        
+        contador2.decrease();
+        contador.increase();
+        
+        System.out.println(contador);
+        System.out.println(contador2);
+        
+        contador2.decrease(5);
+        
+        System.out.println(contador);
+        System.out.println(contador2);
     }
 }
