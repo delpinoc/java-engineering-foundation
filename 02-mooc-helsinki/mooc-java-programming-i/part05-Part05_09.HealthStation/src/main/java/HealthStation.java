79ad7319fdda850ec2ff1feadf2b5c0e80d16cd0
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-14 19:56:22 +0200

    feat(mooc/part5): implement service-layer object interaction and internal state mutation tracking

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,23 @@
 
 public class HealthStation {
 
-
+    private int weighing;
+    
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
+        if (person != null) {
+            this.weighing++;
+            return person.getWeight();
+        }
         return -1;
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.weighing;
     }
 
 }
