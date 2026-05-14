0798d7ff57405602e5fb154d9fc3cdcbae3fa7ae
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-14 21:17:25 +0200

    feat(mooc/part5): implement transaction-based object interaction and stateful terminal logic

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -1,30 +1,67 @@
 
 public class PaymentTerminal {
 
-
     private double money;  // amount of cash
     private int affordableMeals; // number of sold affordable meals
     private int heartyMeals;  // number of sold hearty meals
 
-
     public PaymentTerminal() {
         // register initially has 1000 euros of money
+        this.money = 1000;
     }
 
     public double eatAffordably(double payment) {
         // an affordable meal costs 2.50 euros
         // increase the amount of cash by the price of an affordable mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double affordableMeal = 2.50;
+        if (payment >= affordableMeal) {
+            this.money += affordableMeal;
+            this.affordableMeals++;
+            return payment - affordableMeal;
+        }
+        return payment;
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        double affordableMeal = 2.50;
+        if (card.balance() >= affordableMeal) {
+            card.takeMoney(affordableMeal);
+            this.affordableMeals++;
+            return true;
+        }
+        return false;
     }
 
     public double eatHeartily(double payment) {
         // a hearty meal costs 4.30 euros
         // increase the amount of cash by the price of a hearty mean and return the change
         // if the payment parameter is not large enough, no meal is sold and the method should return the whole payment
-        return -1;
+        double heartyMeal = 4.30;
+        if (payment >= heartyMeal) {
+            this.money += heartyMeal;
+            this.heartyMeals++;
+            return payment - heartyMeal;
+        }
+        return payment;
     }
 
+    public boolean eatHeartily(PaymentCard card) {
+        double heartyMeal = 4.30;
+        if (card.balance() >= heartyMeal) {
+            card.takeMoney(heartyMeal);
+            this.heartyMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (card != null && sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+    }
 
     @Override
     public String toString() {
