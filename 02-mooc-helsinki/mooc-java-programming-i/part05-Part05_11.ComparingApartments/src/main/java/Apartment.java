c88202e9743f2b90bd4ac555bb7925189210a5bc
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-15 00:13:54 +0200

    feat(mooc/part5): implement encapsulated object comparison and differential state evaluation

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -10,5 +10,35 @@ public class Apartment {
         this.squares = squares;
         this.princePerSquare = pricePerSquare;
     }
+    
+    public boolean largerThan(Apartment compared) {
+        if (this.squares > compared.squares) {
+            return true;
+        }
+        return false;
+    }
+    
+    public int priceDifference(Apartment compared) {
+        int priceAp1 = this.squares * this.princePerSquare;
+        int priceAp2 = compared.squares * compared.princePerSquare;
+        
+        int difference = priceAp1 - priceAp2;
+        
+        if (difference < 0) {
+            return difference * -1;
+        }
+        return difference;
+    }
+    
+    public boolean moreExpensiveThan(Apartment compared) {
+        int priceAp1 = this.squares * this.princePerSquare;
+        int priceAp2 = compared.squares * compared.princePerSquare;
+        
+        if (priceAp1 > priceAp2) {
+            return true;
+        }
+        
+        return false;
+    }
 
 }
