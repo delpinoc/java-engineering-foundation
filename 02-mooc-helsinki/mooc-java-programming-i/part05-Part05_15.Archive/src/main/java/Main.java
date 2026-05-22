8e1d8e704a9d8e68f3c1254c5a4cec7a1cf38433
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-05-22 10:06:00 +0200

    feat(mooc/part5): implement ID-based object identity and deduplication logic in Archive catalog

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -7,6 +7,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<Item> items = new ArrayList<>();
 
+        while (true) {
+            System.out.println("Identifier?(empty will stop)");
+            String id = scanner.nextLine();
+
+            if (id.isEmpty()) {
+                break;
+            }
+
+            System.out.println("Name?(empty will stop)");
+            String name = scanner.nextLine();
+
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (!items.contains(new Item(id, name))) {
+                items.add(new Item(id, name));
+            }
+
+        }
+
+        printItems(items);
+
+    }
+
+    public static void printItems(ArrayList<Item> items) {
+        System.out.println("==Items==");
+        for (Item actualItem : items) {
+            System.out.println(actualItem);
+        }
     }
 }
