02b65df740ea0f24acc3c62afb406101cb4efc33
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-06-29 23:56:27 +0200

    feat(mooc/part5): implement immutable value object pattern and defensive state copy for Money entity

--- 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
+++ 02-mooc-helsinki/mooc-java-programming-i/part05-Part05_17.Money/src/main/java/Money.java
@@ -23,6 +23,39 @@ public class Money {
         return this.cents;
     }
 
+    public Money plus(Money addition) {
+        Money newMoney = new Money(this.euros + addition.euros, this.cents + addition.cents);
+        return newMoney;
+    }
+
+    public boolean lessThan(Money compared) {
+        if (this.euros < compared.euros) {
+            return true;
+        }
+
+        if (this.euros == compared.euros && this.cents < compared.cents) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Money minus(Money decreaser) {
+        if (this.lessThan(decreaser)) {
+            return new Money(0, 0);
+        }
+
+        int difEuros = this.euros - decreaser.euros;
+        int difCents = this.cents - decreaser.cents;
+
+        if (difCents < 0) {
+            difEuros--;
+            difCents = 100 + difCents;
+        }
+
+        return new Money(difEuros, difCents);
+    }
+
     public String toString() {
         String zero = "";
         if (this.cents < 10) {
