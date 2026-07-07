11839b23336e26d2de2f01b5c82940a24da754bc
Jorge Del Pino Contreras <jorgemartin.delpinoc@gmail.com>
2026-07-07 10:36:34 +0200

    feat(mooc/part6): separate logic and user interface layers in TodoList

---
+++ 02-mooc-helsinki/mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/UserInterface.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jedcm
+ */
+import java.util.Scanner;
+
+public class UserInterface {
+
+    private TodoList list;
+    private Scanner scanner;
+
+    public UserInterface(TodoList list, Scanner scanner) {
+        this.list = list;
+        this.scanner = scanner;
+    }
+
+    public void start() {
+        
+        while (true) {
+            System.out.println("Command: ");
+            String command = scanner.nextLine();
+
+            if (command.equals("stop")) {
+                break;
+            }
+            
+            if (command.equals("add")) {
+                System.out.println("To add: ");
+                String newTask = scanner.nextLine();
+                list.add(newTask);
+                continue;
+            }
+            
+            if (command.equals("list")) {
+                list.print();
+                continue;
+            }
+            
+            if (command.equals("remove")) {
+                System.out.println("Which one is removed? ");
+                int taskIndex = scanner.nextInt();
+                list.remove(taskIndex);
+                continue;
+            }
+        }
+        
+    }
+
+}
